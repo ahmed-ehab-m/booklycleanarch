cb0951af40258448ac02166bd197debca999a595
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-07-19 20:34:32 +0200

    create fetch featured books use case

---
+++ lib/Features/home/domain/use_cases/fetch_featured_books_use_case.dart
@@ -0,0 +1,12 @@
+import 'package:booklycleanarch/Features/home/domain/entities/book_entity.dart';
+import 'package:booklycleanarch/Features/home/domain/repos/home_repo.dart';
+import 'package:booklycleanarch/core/errors/failure.dart';
+import 'package:dartz/dartz.dart';
+
+class FetchFeaturedBooksUseCase {
+  final HomeRepo homeRepo;
+  FetchFeaturedBooksUseCase(this.homeRepo);
+  Future<Either<Failure, List<BookEntity>>> fetchFeaturedBooks() {
+    return homeRepo.fetchFeaturedBooks();
+  }
+}
