8011795fb31bbbb1aa44ad12a9396d56784609d6
Ahmed Ehab <ahmedehab56200@gmail.com>
2025-07-21 00:23:34 +0200

    implement FetchFeaturedBooksCubit

--- lib/Features/home/presentation/manager/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
+++ lib/Features/home/presentation/manager/fetch_featured_books_cubit/fetch_featured_books_cubit.dart
@@ -1,9 +1,26 @@
 import 'package:bloc/bloc.dart';
 import 'package:booklycleanarch/Features/home/domain/entities/book_entity.dart';
+import 'package:booklycleanarch/Features/home/domain/use_cases/fetch_featured_books_use_case.dart';
 import 'package:meta/meta.dart';
 
 part 'fetch_featured_books_state.dart';
 
 class FetchFeaturedBooksCubit extends Cubit<FetchFeaturedBooksState> {
-  FetchFeaturedBooksCubit() : super(FetchFeaturedBooksInitial());
+  FetchFeaturedBooksCubit(this.fetchFeaturedBooksUseCase)
+    : super(FetchFeaturedBooksInitial());
+  final FetchFeaturedBooksUseCase fetchFeaturedBooksUseCase;
+  ///////////////////////////////////////////////////
+  Future<void> fetchFeaturedBooks() async {
+    emit(FetchFeaturedBooksLoading());
+    var result = await fetchFeaturedBooksUseCase.call();
+    // fold takes 2 anonumous callback functions
+    result.fold(
+      (failure) {
+        emit(FetchFeaturedBooksFailure(failure.message));
+      },
+      (books) {
+        emit(FetchFeaturedBooksSuccess(books));
+      },
+    );
+  }
 }
